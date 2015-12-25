fd69abb7953762b8659dcbc40e55f2f4e1f2c0d6
smeet6 <1445847083@qq.com>
2015-12-25 11:54:59 +0800

    dddd

--- index.js
+++ index.js
@@ -4,43 +4,48 @@ var winW = window.innerWidth;
 var winH = window.innerHeight;
 var desW = 640;
 var desH = 960;
-main.style.webkitTransform = "scale(" + winH / desH + ")";
-/////
-function getEle(ele){
-    return document.querySelector(ele);
+if(winW/winH>desW/desH){
+    main.style.webkitTransform = "scale("+winW/desW+")";
+}else{
+    main.style.webkitTransform = "scale("+winH/desH+")";
 }
-var cube = getEle(".cube");
-var cubeBox = getEle('.cubeBox');
-var cubeLis = document.querySelectorAll('.cubeBox>li');
-function a5(){
-    var startTouch = {x:0,y:0};
-    var startX = -45;
-    var startY = -45;
-    cubeBox.style.webkitTransform = "scale(0.7) rotateX(-45deg) rotateY(-45deg)";
-    [].forEach.call(cubeLis,function(){
-        arguments[0].addEventListener('touchstart',start,false)
-        arguments[0].addEventListener('touchmove',move,false)
-        arguments[0].addEventListener('touchend',end,false);
-    })
-    function start(e){
-        startTouch.x = e.changedTouches[0].pageX;
-        startTouch.y = e.changedTouches[0].pageY;
-    }
-    function move(e){
-        var moveTouchX = e.changedTouches[0].pageX;
-        var moveTouchY = e.changedTouches[0].pageY;
-        this.changePosX = moveTouchX - startTouch.x;
-        this.changePosY = moveTouchY - startTouch.y;
-        this.parentNode.style.webkitTransform = "scale(0.7)  rotateX("+(-startY-this.changePosY)+"deg) rotateY("+(startX+this.changePosX)+"deg)";
-    }
-    function end(){
-        startX+=this.changePosX;
-        startY+=this.changePosY;
-    }
-}
-document.addEventListener('touchstart',function(){
-},false);
-/////
+//main.style.webkitTransform = "scale(" + winH / desH + ")";
+///////
+//function getEle(ele){
+//    return document.querySelector(ele);
+//}
+//var cube = getEle(".cube");
+//var cubeBox = getEle('.cubeBox');
+//var cubeLis = document.querySelectorAll('.cubeBox>li');
+//function a5(){
+//    var startTouch = {x:0,y:0};
+//    var startX = -45;
+//    var startY = -45;
+//    cubeBox.style.webkitTransform = "scale(0.7) rotateX(-45deg) rotateY(-45deg)";
+//    [].forEach.call(cubeLis,function(){
+//        arguments[0].addEventListener('touchstart',start,false)
+//        arguments[0].addEventListener('touchmove',move,false)
+//        arguments[0].addEventListener('touchend',end,false);
+//    })
+//    function start(e){
+//        startTouch.x = e.changedTouches[0].pageX;
+//        startTouch.y = e.changedTouches[0].pageY;
+//    }
+//    function move(e){
+//        var moveTouchX = e.changedTouches[0].pageX;
+//        var moveTouchY = e.changedTouches[0].pageY;
+//        this.changePosX = moveTouchX - startTouch.x;
+//        this.changePosY = moveTouchY - startTouch.y;
+//        this.parentNode.style.webkitTransform = "scale(0.7)  rotateX("+(-startY-this.changePosY)+"deg) rotateY("+(startX+this.changePosX)+"deg)";
+//    }
+//    function end(){
+//        startX+=this.changePosX;
+//        startY+=this.changePosY;
+//    }
+//}
+//document.addEventListener('touchstart',function(){
+//},false);
+///////
 [].forEach.call(oLis, function () {
     arguments[0].index = arguments[1];
     arguments[0].addEventListener('touchstart', start, false);
